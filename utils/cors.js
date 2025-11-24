9403f1e197fe4238b7906d1edf405ad313a3d9fc
shihebaamrii <chihebamri699@gmail.com>
2025-11-24 14:09:36 +0100

    feat: add CORS utility to handle preflight requests and set access control headers for API routes.

--- utils/cors.js
+++ utils/cors.js
@@ -14,6 +14,7 @@ function corsHandler(req, res) {
     'https://kar-be.onrender.com',
     'http://127.0.0.1:5173',
     'http://127.0.0.1:3000',
+    'https://karhabti-com-tn.vercel.app',
     // Add your production frontend URL here when deployed
     // 'https://your-frontend-domain.com'
   ];
